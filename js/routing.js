ba2e5c7b81724d78e7d8dfa27e8482be9356e0fb
Fabian Keller <fabian.keller91@gmail.com>
2014-12-10 22:15:57 +0100

    added sammy routes

--- js/routing.js
+++ js/routing.js
@@ -1,8 +1,7 @@
 (function($) {
-    var app = $.sammy('#main', function(){
+    var app = $.sammy(function(){
 
         this.get('#/', function(){
-            console.log("/#");
             $.ajax({
                 url: 'content/overview.html',
                 cache: false,
@@ -12,7 +11,38 @@
             })
         })
 
+        this.get('#aboutme', function(){
+            $.ajax({
+                url: 'content/aboutme.html',
+                cache: false,
+                success: function(msg){
+                    $("#content").html(msg);
+                }
+            })
+        })
+
+        this.get('#skills', function(){
+            $.ajax({
+                url: 'content/skills.html',
+                cache: false,
+                success: function(msg){
+                    $("#content").html(msg);
+                }
+            })
+        })
+
+        this.get('#contact', function(){
+            $.ajax({
+                url: 'content/contact.html',
+                cache: false,
+                success: function(msg){
+                    $("#content").html(msg);
+                }
+            })
+        })
     });
+
+
     $(function() {
         app.run('#/');
     });
