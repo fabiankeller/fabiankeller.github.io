b2195b59e8b7aad2c93ef20893f7ff7bb6f13faa
Fabian Keller <fabian.keller91@gmail.com>
2014-12-19 23:39:07 +0100

    switched from sammy rounting to single-page selection-scrolling

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by fabiankeller on 19/12/14.
+ */
+
+$(function() {
+    $('.nav a').on('click', function(){
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+});
